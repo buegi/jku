92a8ad514de7af71bcfd3226caea65f012f70cd6
buegi <dev@buegi.com>
2020-03-17 12:16:38 +0100

    ok

--- src/ue02/exceptions/TransportException.java
+++ src/ue02/exceptions/TransportException.java
@@ -1,5 +1,17 @@
 package ue02.exceptions;
 
-public class TransportException extends Exception{
+import ue02.transporters.Transporter;
 
+public class TransportException extends Exception {
+
+    private final Transporter transporter;
+
+    public TransportException(String msg, Transporter transporter) {
+        super(msg);
+        this.transporter = transporter;
+    }
+
+    public Transporter getTransporter() {
+        return this.transporter;
+    }
 }
