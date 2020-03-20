41f91344bb8780c351e7e1238e839e52c8551847
buegi <dev@buegi.com>
2020-03-20 20:10:59 +0100

    ok

--- src/ue01/demo/phonebook/EntryList.java
+++ src/ue01/demo/phonebook/EntryList.java
@@ -1,126 +1,137 @@
 package ue01.demo.phonebook;
 
 /**
- * Class EntryList implements a linked list for phone book entries. Stores the entries in 
- * alphabetical order. 
+ * Class EntryList implements a linked list for phone book entries. Stores the entries in
+ * alphabetical order.
  */
 final class EntryList {
-	
-	/** head of the list */
-	private Node head;
-	
-	/** Constructor */
-	EntryList() {
-		head = null;
-	} 
-	
-	/** 
-	 * Insert a new entry with name, area code and phone number. 
-	 * Maintains an alphabetical order. 
-	 * @param name name of the new entry
-	 * @param areaCode area code of the new entry
-	 * @param number phone number of the new entry
-	 * @return the newly inserted node
-	 */
-	Node insertEntry(String name, String areaCode, String number) {
-		return insertEntry(new Entry(name, areaCode, number)); 
-	}
 
-	/** 
-	 * Inserts an entry value or replaces existing with equal name.  
-	 * Replaces an existing entry with equal name. 
-	 * Maintains an alphabetical order. 
-	 * @param entry the new entry 
-	 * @return the newly inserted node
-	 */
-	Node insertEntry(Entry entry) {
-		Node pred = null;
-		Node curr = head;
+    /**
+     * head of the list
+     */
+    private Node head;
 
-		// find correct position to insert new entry
-		while (curr != null
-				&& curr.entry.getName().compareToIgnoreCase(entry.getName()) < 0) {
-			pred = curr;
-			curr = curr.next;
-		} // while
+    /**
+     * Constructor
+     */
+    EntryList() {
+        head = null;
+    }
 
-		if (curr != null && curr.entry.getName().equalsIgnoreCase(entry.getName())) {
-			// replace entry with equal name
-			curr.entry = entry; 
-			return curr; 
-		} else {
-			// insert new entry in alphabetical order 
-			Node node = new Node(entry);
-			if (pred == null) { // insert in front of head
-				head = node;
-			} else {
-				pred.next = node;
-			}
-			node.next = curr;
-	
-			return node; 
-		}
-	} 
+    /**
+     * Insert a new entry with name, area code and phone number.
+     * Maintains an alphabetical order.
+     *
+     * @param name     name of the new entry
+     * @param areaCode area code of the new entry
+     * @param number   phone number of the new entry
+     * @return the newly inserted node
+     */
+    Node insertEntry(String name, String areaCode, String number) {
+        return insertEntry(new Entry(name, areaCode, number));
+    }
 
-	/**
-	 * Looks up the entry with name. 
-	 * @param name the name of the entry
-	 * @return the entry node found, null if not contained 
-	 */
-	Node lookup(String name) {
-		Node node = head;
+    /**
+     * Inserts an entry value or replaces existing with equal name.
+     * Replaces an existing entry with equal name.
+     * Maintains an alphabetical order.
+     *
+     * @param entry the new entry
+     * @return the newly inserted node
+     */
+    Node insertEntry(Entry entry) {
+        Node pred = null;
+        Node curr = head;
 
-		// search for entry with name >= name
-		while (node != null && node.entry.getName().compareToIgnoreCase(name) < 0) {
-			node = node.next;
-		} // while
+        // find correct position to insert new entry
+        while (curr != null
+                && curr.entry.getName().compareToIgnoreCase(entry.getName()) < 0) {
+            pred = curr;
+            curr = curr.next;
+        } // while
 
-		if (node == null || !node.entry.getName().equalsIgnoreCase(name)) {
-			return null; // not found
-		}
+        if (curr != null && curr.entry.getName().equalsIgnoreCase(entry.getName())) {
+            // replace entry with equal name
+            curr.entry = entry;
+            return curr;
+        } else {
+            // insert new entry in alphabetical order
+            Node node = new Node(entry);
+            if (pred == null) { // insert in front of head
+                head = node;
+            } else {
+                pred.next = node;
+            }
+            node.next = curr;
 
-		return node;
-	} 
+            return node;
+        }
+    }
 
-	/**
-	 * Returns the first entry node in the list.
-	 * @return the first node in the list, null if empty
-	 */
-	Node firstEntry() {
-		return head; 
-	} 
+    /**
+     * Looks up the entry with name.
+     *
+     * @param name the name of the entry
+     * @return the entry node found, null if not contained
+     */
+    Node lookup(String name) {
+        Node node = head;
 
-	/** 
-	 * Returns the next entry node for the given current node. 
-	 * @param current the given current node
-	 * @return the next node after the node current 
-	 */
-	Node nextEntry(Node current) {
-		if (current == null) {
-			return null; // no next entry
-		}
-		return current.next; // return next entry
-	} // nextEntry
+        // search for entry with name >= name
+        while (node != null && node.entry.getName().compareToIgnoreCase(name) < 0) {
+            node = node.next;
+        } // while
 
-	/**
-	 * Returns true if list is empty, false otherwise.  
-	 * @return true if list is empty, false otherwise
-	 */
-	boolean isEmpty() {
-		return head == null; 
-	}
+        if (node == null || !node.entry.getName().equalsIgnoreCase(name)) {
+            return null; // not found
+        }
 
-	/** Deletes the current node. 
-	 * Parameter <code>node</code> must not be null. 
-	 * @param node the node to be deleted
-	 */
-	public void delete(Node node) {
-		if (node == head) head = head.next; 
-		Node prev = head; 
-		while (prev.next != node) {
-			prev = prev.next; 
-		}
-		prev.next = node.next; 
-	}
-	
-} 
+        return node;
+    }
+
+    /**
+     * Returns the first entry node in the list.
+     *
+     * @return the first node in the list, null if empty
+     */
+    Node firstEntry() {
+        return head;
+    }
+
+    /**
+     * Returns the next entry node for the given current node.
+     *
+     * @param current the given current node
+     * @return the next node after the node current
+     */
+    Node nextEntry(Node current) {
+        if (current == null) {
+            return null; // no next entry
+        }
+        return current.next; // return next entry
+    } // nextEntry
+
+    /**
+     * Returns true if list is empty, false otherwise.
+     *
+     * @return true if list is empty, false otherwise
+     */
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * Deletes the current node.
+     * Parameter <code>node</code> must not be null.
+     *
+     * @param node the node to be deleted
+     */
+    public void delete(Node node) {
+        if (node == head) head = head.next;
+        Node prev = head;
+        while (prev.next != node) {
+            prev = prev.next;
+        }
+        prev.next = node.next;
+    }
+}
