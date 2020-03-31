69a3d5b12a46898d5ea7c9e76666e4339cd045e1
buegi <dev@buegi.com>
2020-03-31 20:06:32 +0200

    ok

--- src/ue04/processing/Processors.java
+++ src/ue04/processing/Processors.java
@@ -13,17 +13,74 @@ public class Processors {
     }
 
     // TODO: 1) oeffentliche, abstrakte, statische, innere Klasse "Scaler"
+    public static abstract class Scaler implements Processor {
+
+        private double min, max;
+
+
+        abstract double getMin();
+
+        abstract double getMax();
+
+        @Override
+        Data process(Data data) {
+            // TODO
+
+
+            double scaled = (val - dataMin) / (dataMax - dataMin);
+            double newVal = scaled * (this.getMax() - this.getMin()) + this.getMin();
+
+
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return "[" + this.getMin() + ", " + this.getMax() + "]";
+        }
+    }
 
     // TODO: 2) oeffentliche, statische Methode "Processor scale(double min, double max)"
+    Processor scale(double min, double max) {
+        // TODO
+        return null;
+    }
+
 
     // TODO: 3) oeffentliche, statische Methode "Processor standardize()"
+    public static Processor standardize() {
+        // TODO
+        return null;
+    }
+
 
     // TODO: 4) oeffentliche, statische, innere Klasse "PercentScaler"
+    public static class PercentScaler() extends Scaler {
+
+        final double MIN = 0.0;
+        final double MAX = 100.0;
+
+        @Override
+        double getMin() {
+            return MIN;
+        }
+
+        @Override
+        double getMax() {
+            return MAX;
+        }
+
+
+    }
+
+
 
     // TODO: 5) private, statische, innere Klasse "Clipper"
 
     // TODO: 6) oeffentliche, statische Methode "Processor clip(double lower, double upper)"
+
     // TODO: 6) oeffentliche, statische Methode "Processor clipLower(double lower)"
+
     // TODO: 6) oeffentliche, statische Methode "Processor clipUpper(double upper)"
 
 
