4eef903ca9dcf29a41e3f4082639af81da158c48
buegi <dev@buegi.com>
2020-05-02 18:03:19 +0200

    ok

--- src/ue07/library/Book.java
+++ src/ue07/library/Book.java
@@ -3,33 +3,45 @@ package ue07.library;
 import java.util.Comparator;
 
 public class Book {
-
     private final String name;
-    private int lendCount;
+    private int lentCount;
 
     public Book(String name) {
         this.name = name;
-        this.lendCount = 0;
+        this.lentCount = 0;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public int getLendCount() {
-        return this.lendCount;
+    public int getLentCount() {
+        return this.lentCount;
     }
 
     protected void incLendCount() {
-        this.lendCount++;
+        this.lentCount++;
     }
 
     @Override
     public String toString() {
-        return "Title: " + this.name + ", wurde bisher " + this.lendCount + " mal ausgeliehen.";
+        return "Title: " + this.name + ", wurde bisher " + this.lentCount + " mal ausgeliehen.";
     }
 
+    public static Comparator<Book> getLentComparator() {
+        // TODO
+        /*  public static Comparator<Book> getNameComparator(), liefert einen Comparator, um Bücher nach Namen
+            alphabetisch aufsteigend zu sortieren. Nutzen Sie Comparator.comparing, um einen passenden
+            Comparator zu erzeugen */
+        return null;
+    }
+
+
     public static Comparator<Book> getNameComparator() {
+        // TODO
+        /*  public static Comparator<Book> getLentComparator(),  liefert einen Comparator, um Bücher nach
+            Leihvorgangszähler zu sortieren. Ist der Leihvorgangszähler gleich, soll alphabetisch aufsteigend nach
+            Namen sortiert werden. Implementieren Sie den Comparator als anonyme Klasse direkt in der Methode */
         Comparator<Book> bookNameComparator = Comparator.comparing(Book::getName);
         return bookNameComparator;
     }
--- src/ue07/library/Lender.java
+++ src/ue07/library/Lender.java
@@ -1,4 +1,53 @@
 package ue07.library;
 
-public class Lender {
+import java.util.TreeSet;
+
+public class Lender implements Comparable<Lender> {
+    private final String name;
+    private TreeSet<Book> lentBooks;
+
+    public Lender(String name) {
+        this.name = name;
+        this.lentBooks = new TreeSet<Book>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    protected void addBook(Book newBook) throws IllegalArgumentException {
+        if (this.lentBooks.contains(newBook)) {
+            throw new IllegalArgumentException("Book already lent!");
+        }
+        this.lentBooks.add(newBook);
+    }
+
+    protected void removeBook(String bookName) {
+        // TODO
+    }
+
+
+    @Override
+    public int compareTo(Lender o) {
+        // TODO
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        return null;
+    }
 }
