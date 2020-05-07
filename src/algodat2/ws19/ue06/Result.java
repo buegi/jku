acb5a853538c43d7f269fd4ab2a0aad30dca481c
buegi <dev@buegi.com>
2020-05-07 15:40:54 +0200

    ok

--- src/algodat2/ws19/ue06/Result.java
+++ src/algodat2/ws19/ue06/Result.java
@@ -1,11 +1,11 @@
 package algodat2.ws19.ue06;
 
 public class Result {
-	Integer resultIndices[];
-	Integer count;
+    Integer resultIndices[];
+    Integer count;
 
-	public Result(Integer resultIndices[], int count) {
-		this.resultIndices = resultIndices.clone();
-		this.count = new Integer(count);
-	}
+    public Result(Integer resultIndices[], int count) {
+        this.resultIndices = resultIndices.clone();
+        this.count = Integer.valueOf(count);
+    }
 }
