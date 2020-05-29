bbab147f8c490725d990cfa0fa932661012b28d6
buegi <dev@buegi.com>
2020-05-29 20:46:04 +0200

    ok

--- src/swe2/ss20/ue10/blackjack/ui/BlackJackUI.java
+++ src/swe2/ss20/ue10/blackjack/ui/BlackJackUI.java
@@ -3,6 +3,8 @@ package swe2.ss20.ue10.blackjack.ui;
 import swe2.ss20.ue10.blackjack.game.Blackjack;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class BlackJackUI extends JComponent {
 
@@ -20,18 +22,22 @@ public class BlackJackUI extends JComponent {
     }
 
     private void init() {
-        // initialize MainFrame
         this.initMainFrame();
         this.initMainFrameMenu();
+        this.initDealerPane();
+        this.initPlayerPane();
+        this.initButtonPane();
+        this.initMessagePane();
         mainFrame.setVisible(true);
     }
 
     private void initMainFrame() {
         this.mainFrame = new JFrame();
-        mainFrame.setTitle("Black Jack");
-        mainFrame.setBounds(0, 0, 800, 800);
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.mainFrame.setTitle("Black Jack");
+        this.mainFrame.setBounds(0, 0, 800, 800);
+        this.mainFrame.setLocationRelativeTo(null);
+        this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.mainFrame.setResizable(false);
     }
 
     private void initMainFrameMenu() {
@@ -41,10 +47,52 @@ public class BlackJackUI extends JComponent {
         menuBar.add(menuFile);
         JMenuItem menuItemReset = new JMenuItem("Reset");
         menuFile.add(menuItemReset);
-
         JMenuItem menuItemExit = new JMenuItem("Exit");
-        menuItemExit.addActionListener(ae -> mainFrame.dispose());
+        menuItemExit.addActionListener(ae -> this.mainFrame.dispose());
         menuFile.add(menuItemExit);
     }
 
+    private void initDealerPane() {
+        JPanel dealerPane = new JPanel();
+        dealerPane.setSize(800, 200);
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+        dealerPane.setBorder(blackLine);
+        JLabel dealerLabel = new JLabel("DealerPane ( points )");
+        dealerPane.add(dealerLabel);
+        this.mainFrame.add(dealerPane, BorderLayout.WEST);
+    }
+
+    private void initPlayerPane() {
+        JPanel playerPane = new JPanel();
+        playerPane.setSize(800, 200);
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+        playerPane.setBorder(blackLine);
+        JLabel playerLabel = new JLabel("PlayerPane ( points )");
+        playerPane.add(playerLabel);
+        this.mainFrame.add(playerPane, BorderLayout.EAST);
+    }
+
+    private void initButtonPane() {
+        JPanel buttonPane = new JPanel();
+        buttonPane.setSize(800, 200);
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+        buttonPane.setBorder(blackLine);
+        JButton buttonHit = new JButton("Hit");
+        JButton buttonDoubleDown = new JButton("Double down");
+        JButton buttonStay = new JButton("Stay");
+        buttonPane.add(buttonHit);
+        buttonPane.add(buttonDoubleDown);
+        buttonPane.add(buttonStay);
+        this.mainFrame.add(buttonPane, BorderLayout.NORTH);
+    }
+
+    private void initMessagePane() {
+        JPanel messagePane = new JPanel();
+        messagePane.setSize(800, 200);
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+        messagePane.setBorder(blackLine);
+        JLabel messageLabel = new JLabel("This is a message, that something's going on!");
+        messagePane.add(messageLabel);
+        this.mainFrame.add(messagePane, BorderLayout.SOUTH);
+    }
 }
