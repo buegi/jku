14978c0c5fc0658ae6dec916a1d6b01af99b9d3f
buegi <dev@buegi.com>
2020-06-18 03:07:17 +0200

    ue11

--- src/swe2/ss20/ue11/graphiceditor/decorator/FrameDecorator.java
+++ src/swe2/ss20/ue11/graphiceditor/decorator/FrameDecorator.java
@@ -2,12 +2,12 @@ package swe2.ss20.ue11.graphiceditor.decorator;
 
 import swe2.ss20.ue11.graphiceditor.objects.GraphicObject;
 import swe2.ss20.ue11.graphiceditor.visitor.GraphicObjectVisitor;
-import swe2.ss20.ue11.graphiceditor.visitor.GraphicObjectVisitor;
 
 import java.awt.*;
 
-
 public class FrameDecorator extends AbstractDecorator {
+
+    private final int BORDER_THICKNESS = 3;
 
     public FrameDecorator(GraphicObject go) {
         super(go);
@@ -16,10 +16,10 @@ public class FrameDecorator extends AbstractDecorator {
     @Override
     public void paint(Graphics g) {
         go.paint(g);
-
-        g.setColor(Color.BLACK);
-        g.drawRect(go.getX() - go.getWidth() / 2, go.getY() - go.getHeight() / 2, go.getWidth(), go.getHeight());
-
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setStroke(new BasicStroke(BORDER_THICKNESS));
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect((go.getX() + BORDER_THICKNESS) - (go.getWidth() + BORDER_THICKNESS * 2) / 2, (go.getY() + BORDER_THICKNESS) - (go.getHeight() + BORDER_THICKNESS * 2) / 2, go.getWidth(), go.getHeight());
     }
 
     @Override
