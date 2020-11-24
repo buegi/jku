9dbc9e880bbe70f5e3aa0a2316025fca949b5538
buegi <dev@buegi.com>
2020-11-24 21:25:32 +0100

    ok

--- src/tests/shiftright/ShiftRight.java
+++ src/tests/shiftright/ShiftRight.java
@@ -5,18 +5,16 @@ public class ShiftRight {
     public static void main(String[] args) {
         int[] test = {1, 2, 3, 4, 5};
         int places = 2;
-
         printArray(test);
         int[] shifted = shiftWithDrop(test, places);
-
         printArray(shifted);
     }
 
     static int[] shiftWithDrop(int[] field, int places) {
         // shift right
-        for (int i = 0; i < places;i ++) {
-            for (int index = field.length - 2; index >= 0; index--) {
-                field[index + 1] = field[index];
+        for (int i = 0; i < places; i++) {
+            for (int j = field.length - 2; j >= 0; j--) {
+                field[j + 1] = field[j];
             }
             field[i] = 0;
         }
