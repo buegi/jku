5651a38efcd3b35d45c6633857eae210a7384698
buegi <dev@buegi.com>
2021-04-08 20:03:40 +0200

    pr.swe2.ue03

--- src/prswe2/ss21/ue03/gradetable/controller/Controller.java
+++ src/prswe2/ss21/ue03/gradetable/controller/Controller.java
@@ -2,12 +2,19 @@ package prswe2.ss21.ue03.gradetable.controller;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import prswe2.ss21.ue03.gradetable.model.GradeTableModel;
 import prswe2.ss21.ue03.gradetable.model.Results;
+import prswe2.ss21.ue03.gradetable.model.Student;
 
 import java.util.Arrays;
 
@@ -38,15 +45,26 @@ public class Controller {
     private TableColumn<Results, String>[] assignmentColumns = new TableColumn[Results.NR_ASSIGNMENTS];
 
     @FXML
+    private Button addBtn;
+    @FXML
+    private Button removeBtn;
+
+    @FXML
     private TableColumn<Results, String> sumColumn;
 
     @FXML
     private TableColumn<Results, String> gradeColumn;
 
     @FXML
-    private Button addBtn;
+    private TextField popUpAddStudentButtonId = new TextField();
     @FXML
-    private Button removeBtn;
+    private TextField popUpAddStudentButtonName = new TextField();
+    @FXML
+    private TextField popUpAddStudentButtonFirst = new TextField();
+    @FXML
+    private ComboBox<Integer> popUpAddStudentButtonSnComboBox = new ComboBox<>();
+    @FXML
+    private Button popUpAddStudentButton = new Button("Add");
 
     @FXML
     private void initialize() {
@@ -65,7 +83,7 @@ public class Controller {
             assignmentColumns[i] = new TableColumn<>("A" + (i + 1));
             final int a = i;
             assignmentColumns[i].setCellValueFactory(as -> new SimpleStringProperty(Integer.toString(as.getValue().getAssignment(a))));
-            assignmentColumns[i].setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<String>() {
+            assignmentColumns[i].setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<>() {
                 @Override
                 public String toString(String s) {
                     return s != null ? s : "";
@@ -85,14 +103,44 @@ public class Controller {
 
         addBtn.setOnAction(e -> {
             System.out.println("add");
+            addStudentWindow();
         });
 
         removeBtn.setOnAction(e -> {
             System.out.println("remove");
             if (resultsView.getSelectionModel().getSelectedIndex() >= 0)
                 model.results.remove(resultsView.getSelectionModel().getSelectedIndex());
+            // check if model only contains non deleted items
+            model.results.forEach(r -> System.out.println(r.getStudent().nameProperty()));
         });
 
+        popUpAddStudentButton.setOnAction(e -> {
+            model.results.add(new Results(new Student(popUpAddStudentButtonId.getText(), popUpAddStudentButtonFirst.getText(),
+                    popUpAddStudentButtonName.getText(), popUpAddStudentButtonSnComboBox.getValue())));
+        });
+    }
+
+    private void addStudentWindow() {
+        VBox vbox1 = new VBox(1);
+        VBox vbox2 = new VBox(1);
+        HBox hbox = new HBox(1);
+        vbox1.getChildren().addAll(new Text("Student ID:"), new Text("Student Name:"),
+                new Text("Student First Name:"), new Text("Student Study Number"));
+        vbox2.getChildren().addAll(popUpAddStudentButtonId, popUpAddStudentButtonName, popUpAddStudentButtonFirst,
+                popUpAddStudentButtonSnComboBox, popUpAddStudentButton);
+        hbox.getChildren().addAll(vbox1, vbox2);
+        popUpAddStudentButtonSnComboBox.getItems().addAll(521, 921, 822);
+        popUpAddStudentButtonSnComboBox.getSelectionModel().selectFirst();
+        StackPane root = new StackPane();
+        root.getChildren().add(hbox);
+        /* new window with content */
+        Stage popupStage = new Stage();
+        popupStage.setTitle("Add Student");
+        popupStage.setScene(new Scene(root, 300, 200));
+        /* prevent access to main window while open */
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        /* show popup window */
+        popupStage.show();
     }
 
     public void setPrimaryStage(Stage primaryStage) {
