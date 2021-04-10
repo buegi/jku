58dfef2d18e1b3eb9a078ebeebd1cdb3420ff3c3
buegi <dev@buegi.com>
2021-04-10 15:28:34 +0200

    pr.swe2.ue03

--- src/prswe2/ss21/ue03/gradetable/controller/AddStudentController.java
+++ src/prswe2/ss21/ue03/gradetable/controller/AddStudentController.java
@@ -1,28 +1,16 @@
 package prswe2.ss21.ue03.gradetable.controller;
 
-import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 import prswe2.ss21.ue03.gradetable.model.GradeTableModel;
 import prswe2.ss21.ue03.gradetable.model.Results;
 import prswe2.ss21.ue03.gradetable.model.Student;
 
 public class AddStudentController {
 
-    private Stage primaryStage;
-
-    public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
+    private GradeTableModel model;
 
     @FXML
     Label idLabel = new Label("Student ID: ");
@@ -49,52 +37,40 @@ public class AddStudentController {
     private ComboBox<Integer> snComboBox = new ComboBox<>();
 
     @FXML
+    Label errorLabel = new Label("Empty Fields");
+
+    @FXML
     private Button addButton = new Button("Add");
 
     @FXML
     private void initialize() {
 
+        snComboBox.getItems().addAll(521, 526, 921);
+        errorLabel.setVisible(false);
+        errorLabel.setTextFill(Color.RED);
+
+
         addButton.setOnAction(e -> {
-//            Stage addStudentUI = new Stage();
-//            addStudentUI.initOwner(primaryStage);
-//            final FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/addStudentUI.fxml"));
-//            addStudentUI.setTitle("Add Student");
-//            final Parent root;
-//            try {
-//                root = loader.load();
-//                addStudentUI.setScene(new Scene(root));
-//            } catch (IOException ioException) {
-//                ioException.printStackTrace();
-//            }
-//            addStudentUI.show();
-//            Stage popupAddStudentStage = new Stage();
-//            VBox vbox1 = new VBox(10);
-//            VBox vbox2 = new VBox(10);
-//            HBox hbox = new HBox(10);
-//
-//            vbox1.getChildren().addAll(new Text("Student ID:"), new Text("Student Name:"),
-//                    new Text("Student First Name:"), new Text("Student Study Number"));
-//            vbox2.getChildren().addAll(popUpAddStudentButtonId, popUpAddStudentButtonName, popUpAddStudentButtonFirst,
-//                    popUpAddStudentButtonSnComboBox, popUpAddStudentButton);
-//            hbox.getChildren().addAll(vbox1, vbox2);
-//            popUpAddStudentButtonSnComboBox.getItems().addAll(521, 921, 822);
-//            popUpAddStudentButtonSnComboBox.getSelectionModel().selectFirst();
-//            StackPane root = new StackPane();
-//            root.getChildren().add(hbox);
-//            popupAddStudentStage.setTitle("Add Student");
-//            popupAddStudentStage.setScene(new Scene(root, 300, 200));
-//            popupAddStudentStage.initModality(Modality.APPLICATION_MODAL);
-//            popupAddStudentStage.show();
-//
-//            popUpAddStudentButton.setOnAction(ev -> {
-//                Results result = new Results(new Student(popUpAddStudentButtonId.getText(), popUpAddStudentButtonFirst.getText(),
-//                        popUpAddStudentButtonName.getText(), popUpAddStudentButtonSnComboBox.getValue()));
-//                // check newly added result/student
-//                System.out.println(result);
-//                model.results.add(result);
-//                popupAddStudentStage.close();
-//            });
-//        });
+            if (idTextField.getText() == null || idTextField.getText().trim().isEmpty()
+                    || nameTextField.getText() == null || nameTextField.getText().trim().isEmpty()
+                    || firstTextField.getText() == null || firstTextField.getText().trim().isEmpty()
+                    || snComboBox.getSelectionModel().isEmpty()) {
+                errorLabel.setVisible(true);
+
+            } else {
+                errorLabel.setVisible(false);
+                Results result = new Results(new Student(idTextField.getText(), nameTextField.getText(),
+                        firstTextField.getText(), snComboBox.getValue()));
+                System.out.println(result);
+                model.results.add(result);
+
+                Stage stage = (Stage) addButton.getScene().getWindow();
+                stage.close();
+            }
         });
+    }
+
+    public void initModel(GradeTableModel model) {
+        this.model = model;
     }
 }
--- src/prswe2/ss21/ue03/gradetable/controller/GradeTableController.java
+++ src/prswe2/ss21/ue03/gradetable/controller/GradeTableController.java
@@ -2,19 +2,17 @@ package prswe2.ss21.ue03.gradetable.controller;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import prswe2.ss21.ue03.gradetable.model.GradeTableModel;
 import prswe2.ss21.ue03.gradetable.model.Results;
-import prswe2.ss21.ue03.gradetable.model.Student;
+
+import java.io.IOException;
 
 public class GradeTableController {
 
@@ -55,23 +53,12 @@ public class GradeTableController {
     @FXML
     private TableColumn<Results, String> gradeColumn;
 
-    private TextField popUpAddStudentButtonId = new TextField();
-
-    private TextField popUpAddStudentButtonName = new TextField();
-
-    private TextField popUpAddStudentButtonFirst = new TextField();
-
-    private ComboBox<Integer> popUpAddStudentButtonSnComboBox = new ComboBox<>();
-
-    private Button popUpAddStudentButton = new Button("Add");
-
     @FXML
     private void initialize() {
         resultsView.itemsProperty().setValue(model.results);
         resultsView.setEditable(true);
         resultsView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-        // student values
         idColumn.setCellValueFactory(r -> r.getValue().getStudent().idProperty());
         nameColumn.setCellValueFactory(r -> r.getValue().getStudent().nameProperty());
         firstNameColumn.setCellValueFactory(r -> r.getValue().getStudent().firstNameProperty());
@@ -112,59 +99,29 @@ public class GradeTableController {
             }));
         }
 
-
-        // grade values
         sumColumn.setCellValueFactory(r -> r.getValue().getGradePoints().asObject());
         gradeColumn.setCellValueFactory(r -> r.getValue().getGrade());
 
         addBtn.setOnAction(e -> {
-//            Stage addStudentUI = new Stage();
-//            addStudentUI.initOwner(primaryStage);
-//            final FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/addStudentUI.fxml"));
-//            addStudentUI.setTitle("Add Student");
-//            final Parent root;
-//            try {
-//                root = loader.load();
-//                addStudentUI.setScene(new Scene(root));
-//            } catch (IOException ioException) {
-//                ioException.printStackTrace();
-//            }
-//            addStudentUI.show();
-            Stage popupAddStudentStage = new Stage();
-            VBox vbox1 = new VBox(10);
-            VBox vbox2 = new VBox(10);
-            HBox hbox = new HBox(10);
-
-            vbox1.getChildren().addAll(new Text("Student ID:"), new Text("Student Name:"),
-                    new Text("Student First Name:"), new Text("Student Study Number"));
-            vbox2.getChildren().addAll(popUpAddStudentButtonId, popUpAddStudentButtonName, popUpAddStudentButtonFirst,
-                    popUpAddStudentButtonSnComboBox, popUpAddStudentButton);
-            hbox.getChildren().addAll(vbox1, vbox2);
-            popUpAddStudentButtonSnComboBox.getItems().addAll(521, 921, 822);
-            popUpAddStudentButtonSnComboBox.getSelectionModel().selectFirst();
-            StackPane root = new StackPane();
-            root.getChildren().add(hbox);
-            popupAddStudentStage.setTitle("Add Student");
-            popupAddStudentStage.setScene(new Scene(root, 300, 200));
-            popupAddStudentStage.initModality(Modality.APPLICATION_MODAL);
-            popupAddStudentStage.show();
-
-            popUpAddStudentButton.setOnAction(ev -> {
-                Results result = new Results(new Student(popUpAddStudentButtonId.getText(), popUpAddStudentButtonFirst.getText(),
-                        popUpAddStudentButtonName.getText(), popUpAddStudentButtonSnComboBox.getValue()));
-                // check newly added result/student
-                System.out.println(result);
-                model.results.add(result);
-                popupAddStudentStage.close();
-            });
+            Stage addStudentUI = new Stage();
+            addStudentUI.initOwner(primaryStage);
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/addStudentUI.fxml"));
+            addStudentUI.setTitle("Add Student");
+            final Parent root;
+            try {
+                root = loader.load();
+                addStudentUI.setScene(new Scene(root));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            AddStudentController addStudentController = loader.getController();
+            addStudentController.initModel(this.model);
+            addStudentUI.show();
         });
 
         removeBtn.setOnAction(e -> {
             if (resultsView.getSelectionModel().getSelectedIndex() >= 0)
                 model.results.remove(resultsView.getSelectionModel().getSelectedIndex());
-            // check if model only contains non deleted items
-            System.out.println("Items remaining after remove:");
-            model.results.forEach(r -> System.out.println(r.getStudent().nameProperty()));
         });
     }
 }
