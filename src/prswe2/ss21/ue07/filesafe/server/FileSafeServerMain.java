f45aecc14ae0dc22c3e1935eef5ffbc7644d6fbd
buegi <dev@buegi.com>
2021-05-29 00:56:12 +0200

    pr.swe2.ue07

---
+++ src/prswe2/ss21/ue07/filesafe/server/FileSafeServerMain.java
@@ -0,0 +1,17 @@
+package prswe2.ss21.ue07.filesafe.server;
+
+import static prswe2.ss21.ue07.filesafe.protocol.Constants.*;
+
+public class FileSafeServerMain {
+
+    public static void main(String[] args) {
+
+        FileSafeServer fileSafeServer;
+        if (SYNC_SERVER) {
+            fileSafeServer = new SyncFileSafeServer();
+        } else {
+            fileSafeServer = new AsyncFileSafeServer();
+        }
+        fileSafeServer.init();
+    }
+}
