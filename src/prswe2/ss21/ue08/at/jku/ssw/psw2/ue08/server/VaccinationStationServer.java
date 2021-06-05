1df41e9f0f9059bd0ba7b99cecc8c4d522973503
buegi <dev@buegi.com>
2021-06-05 09:26:50 +0200

    pr.swe2.ue08

---
+++ src/prswe2/ss21/ue08/at/jku/ssw/psw2/ue08/server/VaccinationStationServer.java
@@ -0,0 +1,16 @@
+package prswe2.ss21.ue08.at.jku.ssw.psw2.ue08.server;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class VaccinationStationServer {
+
+
+    public static void main(String[] args) {
+
+    }
+}
